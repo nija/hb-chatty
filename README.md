17c28f034846537ebb980300f427482d4093d95b
Nija Mashruwala <nija.mashruwala@gmail.com>
2016-05-07 07:44:50 -0700

    Initial commit for testing CD

---
+++ README.md
@@ -0,0 +1,3 @@
+# hb-chatty
+
+<a href='https://coveralls.io/github/nija/hb-chatty?branch=master'><img src='https://coveralls.io/repos/github/nija/hb-chatty/badge.svg?branch=master' alt='Coverage Status' /></a>
--- tests.py
+++ tests.py
@@ -16,14 +16,14 @@ class ChatTests(unittest.TestCase):
         self.assertIn("Please chat with me", result.data)
         self.assertEqual(result.status_code, 200)
 
-    def test_rsvp(self):
-        '''Checking... post something something'''
-        result = self.client.post("/process_msg_event",
-                                  data={'name': "Jane", 'data': "jane@jane.com"},
-                                  follow_redirects=True)
+    # def test_rsvp(self):
+    #     '''Checking... post something something'''
+    #     result = self.client.post("/process_msg_event",
+    #                               data={'name': "Jane", 'data': "jane@jane.com"},
+    #                               follow_redirects=True)
 
-        self.assertIn("jane@jane", result.data)
-        self.assertEqual(result.status_code, 200)
+    #     self.assertIn("jane@jane", result.data)
+    #     self.assertEqual(result.status_code, 200)
         #self.assertNotIn("Please RSVP", result.data)
 
 
